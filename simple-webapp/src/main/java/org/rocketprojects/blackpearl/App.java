fe3cd53329cebb12ff6aa04be868e856b141b734
arun <arunkumar.poh@gmail.com>
2016-02-05 01:28:23 -0800

    First set of changes

---
+++ simple-webapp/src/main/java/org/rocketprojects/blackpearl/App.java
@@ -0,0 +1,13 @@
+package org.rocketprojects.blackpearl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
